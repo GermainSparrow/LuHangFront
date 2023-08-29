d5fb33d1fc4cc1fe96ad80848c35781a3bd31e6a
sam.lai <sam.lai@silksoftware.com>
2023-08-29 16:22:08 +0800

    just start

---
+++ ChallengeYourself/src/LayoutRoutes.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+import {
+    useLocation,
+    useHistory,
+    Switch,
+    Route,
+    Redirect,
+} from 'react-router-dom'
+export const LayoutRoutes: FC = {
+    return 
+}
